e6f4a32eba0dbdafe1a10916e2db4e323937f3db
Pippin Williamson <pippin@pippinsplugins.com>
2014-02-09 17:12:33 -0600

    Snippet for modifying required fields

---
+++ checkout/required-fields.php
@@ -0,0 +1,19 @@
+<?php
+ 
+/*
+ * Modify the required fields for checkout
+ *
+ * Fields are identified by the input fields "name" attribute.
+ *
+ * This example shows how to make the last name field, which has a name attribute of "edd_last", required
+ */
+function pw_edd_purchase_form_required_fields( $required_fields ) {
+ 
+    $required_fields['edd_last'] = array(   
+        'error_id'      => 'invalid_last_name',
+        'error_message' => __( 'Please enter your last name.', 'edd' )
+    );
+ 
+    return $required_fields;
+}
+add_filter( 'edd_purchase_form_required_fields', 'pw_edd_purchase_form_required_fields' );
