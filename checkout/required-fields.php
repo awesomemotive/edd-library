ae51b891e7e7999fcbd783e5daf8d414202bd7d4
Pippin Williamson <pippin@pippinsplugins.com>
2014-02-09 17:13:39 -0600

    show example of how to remove a required field

--- checkout/required-fields.php
+++ checkout/required-fields.php
@@ -9,10 +9,17 @@
  */
 function pw_edd_purchase_form_required_fields( $required_fields ) {
  
+ 	// Set a field as required
     $required_fields['edd_last'] = array(   
         'error_id'      => 'invalid_last_name',
         'error_message' => __( 'Please enter your last name.', 'edd' )
     );
+
+    /*
+     * If you wanted to remove a field from required list
+	 * you would simply unset the array key
+	 */
+    // unset( $required_fields['edd_first'] );
  
     return $required_fields;
 }
