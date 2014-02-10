8f64b4e65c9bbd2d0ab459b75e247c1a8fd1dcf1
Chris Klosowski <cklosowski@gmail.com>
2014-02-09 19:03:20 -0700

    Adding snippet for altering the payment list quick actions

--- admin/alter-payment-list-quick-actions.php
+++ admin/alter-payment-list-quick-actions.php
@@ -1,8 +1,10 @@
 <?php
 
 /* Remove the 'Delete' option from the Payment List Quick Action
- * This requires a fine tuning of the user roles, typically handeled in a plugin
+ * This can require a fine tuning of the user roles, typically handeled in a plugin
  * Example: http://wordpress.org/plugins/user-role-editor/
+ *
+ * Source of snippet: https://easydigitaldownloads.com/support/topic/custom-payment-history-view-per-user-role/
  */
 
 function ck_edd_remove_payment_delete_action( $row_actions, $payment ) {
