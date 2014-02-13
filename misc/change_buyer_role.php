4e52569c1b073ffc63c4c5f5c9e81f02f887b9aa
Pippin Williamson <pippin@pippinsplugins.com>
2014-02-13 11:51:09 -0600

    Cleanup customer role snippet. #4

--- misc/change_buyer_role.php
+++ misc/change_buyer_role.php
@@ -7,23 +7,16 @@
   * (buyers-only support forums, tutorials, etc...)
   *
   */
-  
-// update user role from "subscriber" to "buyer" when purchase is completed
-function ao_edd_run_when_purchase_complete( $payment_id ) {
-        $email = edd_get_payment_user_email( $payment_id );
-        $downloads = edd_get_payment_meta_downloads( $payment_id );
-        $user_id   = edd_get_payment_user_id( $payment_id );
-        if( is_array( $downloads ) ) {
-// Increase purchase count and earnings
-        foreach( $downloads as $download ) {
-// Get product ID
-        $download_id = $download['340'];
-// Do stuff here for each product purchased
-        $user = new WP_User( $user_id );
-       // Add role
-       $user->add_role( 'buyer' );
-        }
- }
-}
+function ao_edd_set_customer_role( $payment_id ) {
 
-add_action( 'edd_complete_purchase', 'ao_edd_run_when_purchase_complete', 100, 3 );
+	$email     = edd_get_payment_user_email( $payment_id );
+	$downloads = edd_get_payment_meta_downloads( $payment_id );
+	$user_id   = edd_get_payment_user_id( $payment_id );
+
+	if( $user_id ) {
+		$user = new WP_User( $user_id );
+		// Add role
+		$user->add_role( 'buyer' );
+	}
+}
+add_action( 'edd_complete_purchase', 'ao_edd_set_customer_role', 100 );
