60e9d58b006226a644401913e85b64a5be2549aa
Lorenzo Novaro <novalore@19.coop>
2014-02-13 13:42:21 +0100

    Changing $user_id retrieval and hook
    following Pippin's suggestions

--- misc/change_buyer_role.php
+++ misc/change_buyer_role.php
@@ -12,27 +12,22 @@
 function ao_edd_run_when_purchase_complete( $payment_id, $new_status, $old_status ) {
         if( $old_status == 'publish' || $old_status == 'complete' )
         {return;} // Make sure that payments are only completed once
-// Make sure the payment completion is only processed when new status is complete
-        if( $new_status != 'publish' && $new_status != 'complete' )
-        {return;}
         $payment_data = edd_get_payment_meta( $payment_id );
         $email = maybe_unserialize( $payment_data['user_email'] );
         $downloads = maybe_unserialize( $payment_data['downloads'] );
-        $user_info = maybe_unserialize( $payment_data['user_info'] );
         $cart_details = maybe_unserialize( $payment_data['cart_details'] );
+        $user_id   = edd_get_payment_user_id( $payment_id );
         if( is_array( $downloads ) ) {
 // Increase purchase count and earnings
         foreach( $downloads as $download ) {
 // Get product ID
         $download_id = $download['340'];
 // Do stuff here for each product purchased
-        global $current_user;
-        get_currentuserinfo($user_id);
-        $user = new WP_User($user_info['id']);
+        $user = new WP_User( $user_id );
        // Add role
        $user->add_role( 'buyer' );
         }
  }
 }
 
-add_action( 'edd_update_payment_status', 'ao_edd_run_when_purchase_complete', 100, 3 );
+add_action( 'edd_complete_purchase', 'ao_edd_run_when_purchase_complete', 100, 3 );
