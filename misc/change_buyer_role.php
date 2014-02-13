39368637c0acf3bc3cb6a440330f4040a3ef98f8
Lorenzo Novaro <novalore@19.coop>
2014-02-13 16:27:48 +0100

    cleaning leftovers as suggested

--- misc/change_buyer_role.php
+++ misc/change_buyer_role.php
@@ -9,13 +9,9 @@
   */
   
 // update user role from "subscriber" to "buyer" when purchase is completed
-function ao_edd_run_when_purchase_complete( $payment_id, $old_status ) {
-        if( $old_status == 'publish' || $old_status == 'complete' )
-        {return;} // Make sure that payments are only completed once
-        $payment_data = edd_get_payment_meta( $payment_id );
-        $email = maybe_unserialize( $payment_data['user_email'] );
-        $downloads = maybe_unserialize( $payment_data['downloads'] );
-        $cart_details = maybe_unserialize( $payment_data['cart_details'] );
+function ao_edd_run_when_purchase_complete( $payment_id ) {
+        $email = edd_get_payment_user_email( $payment_id );
+        $downloads = edd_get_payment_meta_downloads( $payment_id );
         $user_id   = edd_get_payment_user_id( $payment_id );
         if( is_array( $downloads ) ) {
 // Increase purchase count and earnings
