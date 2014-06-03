ea285417e82bac51355c624b6200ec250845d2fa
Chris Klosowski <cklosowski@gmail.com>
2014-06-03 06:47:35 -0700

    Plugin format

--- checkout/continue-shopping-button.php
+++ checkout/continue-shopping-button.php
@@ -1,7 +1,11 @@
 <?php
-/**
- * Adds a 'Continue Shopping' button to the left of the Update and Save Cart Buttons on checkout
+/*
+ * Plugin Name: Continue Shopping Button
+ * Description: Adds a continue shopping button to the left of the Update/Save Cart buttons on checkout
+ * Author: Chirs Klosowski
+ * Version: 1.0
  */
+
 add_action( 'edd_cart_footer_buttons', 'ck_edd_continue_shopping_button', 1 );
 function ck_edd_continue_shopping_button() {
 	global $edd_options;
