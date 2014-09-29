0a1c32208c03a9c25390028f360b252d85784874
Pippin Williamson <pippin@pippinsplugins.com>
2014-09-29 11:41:27 -0500

    Add sample plugin for making PDF Invoices shiw up on pending items

---
+++ extensions/pdf-invoices/show-invoices-for-pending.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name: Easy Digital Downloads - PDF Invoices - Show for Pending
+ * Description: Makes PDF Invoices available for pending payments
+ * Author: Pippin Williamson
+ * Version: 1.0
+ */
+
+function pw_edd_pdf_invoices_for_pending( $show_invoice, $payment_id ) {
+	
+	$status = edd_get_payment_status( $payment_id );
+
+	if( 'pending' === $status ) {
+		$show_invoice = true;
+	}
+
+	return $show_invoice;
+}
+add_filter( 'eddpdfi_is_invoice_link_allowed', 'pw_edd_pdf_invoices_for_pending', 10, 2 );
