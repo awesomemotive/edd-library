da55964cd15e72442093a76ef0c704c18376aa07
Kyle <kyle@realbigmarketing.com>
2015-06-20 12:25:34 -0400

    Create change-vendor-pending-message.php
    Changes the message displayed to vendors after they apply and are redirected.

---
+++ extensions/frontend-submissions/change-vendor-pending-message.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Change Vendor Pending Message
+* Author: Kyle Maurer
+*/
+function kjm_change_vendor_pending_message() {
+	return 'My new message!';
+}
+add_action( 'fes_application_pending_message', 'kjm_change_vendor_pending_message' );
