7c8fad13c2dfccd5513a919a8a06c6b8244b0cb4
Pippin Williamson <pippin@pippinsplugins.com>
2015-11-12 15:09:25 -0600

    Add snippet to include subscription payments in /sales/ API endpoint

---
+++ extensions/recurring-payments/sub-payments-in-api.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Include EDD Subscription payments in the /edd-api/sales endpoint
+ *
+ */
+function pw_edd_recurring_include_payments_in_api( $query ) {
+  
+  if( ! defined( 'EDD_DOING_API' ) || ! EDD_DOING_API ) {
+    return;
+  }
+
+  $query->__set( 'post_status', array( 'complete', 'edd_subscription' ) );
+
+}
+add_action( 'edd_pre_get_payments', 'pw_edd_recurring_include_payments_in_api' );
