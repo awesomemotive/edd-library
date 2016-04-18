dff997b77316967c0b23e7e5a7b713fe6f230d27
Pippin Williamson <pippin@pippinsplugins.com>
2016-04-18 10:43:53 -0500

    Add snippet for grand fathering in renewal discounts for customers that purchased before a set date

---
+++ extensions/software-licensing/grandfather-renewal-rate.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Sets renewal discount to 40% for any customer that purchased before April 18, 2016
+ */
+function pw_edd_grandfather_renewal_discount( $renewal_discount, $license_id ) {
+
+	$license = get_post( $license_id );
+
+	if( strtotime( $license->post_date ) < strtotime( 'April 18, 2016' ) ) {
+		$discount = 40;
+	}
+
+	return $discount;
+}
+add_filter( 'edd_sl_renewal_discount_percentage', 'pw_edd_grandfather_renewal_discount', 10, 2 );
