0fd697c4b147210eef4cf1cfd7e653248f55eb28
Chris Klosowski <cklosowski@gmail.com>
2017-11-27 10:24:59 -0700

    Add plugin headers to prevent-duplicate-cart-items.php

--- checkout/prevent-duplicate-cart-items.php
+++ checkout/prevent-duplicate-cart-items.php
@@ -1,9 +1,12 @@
 <?php
 
-/**
- * Prevents items from being added to the cart multiple times
- *
+/*
+ * Plugin Name: Easy Digital Downloads - Prevent Duplciate Cart Items
+ * Description: Prevents the same item from being added to the cart multiple times.
+ * Author: EDD Team
+ * Version: 1.0
  */
+
 function pw_edd_prevent_duplicate_cart_items( $download_id, $options ) {
 	if( edd_item_in_cart( $download_id, $options ) ) {
 		if( edd_is_ajax_enabled() && defined('DOING_AJAX') && DOING_AJAX ) {
